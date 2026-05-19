d22cb360ae8895a97a061226f1d3faed14dd0f6d
Maurice-H <maizuou.spam+github@gmail.com>
2026-05-19 08:02:32 +0200

    chore: clean barrel exports, unused types, CSS dedup
    - Remove dead exports from navigation/index.ts and shared/index.ts
    - Remove unused interfaces: BentoCardProps, BentoGridItem, MousePosition,
      HeroData, ToastOptions from types/index.ts
    - Remove SKILLS_DEFAULT_VISIBLE constant from portfolio.ts
    - Remove duplicate .flash-overlay CSS from index.css

--- src/components/navigation/index.ts
+++ src/components/navigation/index.ts
@@ -1,4 +1,2 @@
-export { default as LightingToggle } from './LightingToggle.vue';
 export { default as NavConveyor } from './NavConveyor.vue';
 export { default as NavWindow } from './NavWindow.vue';
-export { default as ThemeToggle } from './ThemeToggle.vue';
